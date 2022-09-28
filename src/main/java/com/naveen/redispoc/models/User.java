824bca5e882af6858487250088e78247ac9141b8
Naveen <naveen.chaurasia861@gmail.com>
2022-09-28 09:05:43 +0530

    added user entity

--- src/main/java/com/naveen/redispoc/models/User.java
+++ src/main/java/com/naveen/redispoc/models/User.java
@@ -1,5 +1,36 @@
 package com.naveen.redispoc.models;
 
-public class User {
+import javax.persistence.*;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 7156526077883281623L;
+
+    @Id
+    @SequenceGenerator(name = "SEQ_GEN", sequenceName = "SEQ_USER", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GEN")
+    private Long id;
+    private String name;
+    private long followers;
+
+    public User() {
+    }
+
+    public User(String name, long followers) {
+        this.name = name;
+        this.followers = followers;
+    }
+
+    //standard getters and setters
+
+    @Override
+    public String toString() {
+        return String.format("User{id=%d, name='%s', followers=%d}", id, name, followers);
+    }
 }
