5b94b4736410e2e83eb3b4ac0c081b29d78d7adb
Naveen <naveen.chaurasia861@gmail.com>
2022-09-29 11:36:37 +0530

    added redis config

---
+++ src/main/java/com/naveen/redispoc/configuration/RedisConfig.java
@@ -0,0 +1,43 @@
+package com.naveen.redispoc.configuration;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.jcache.config.JCacheConfigurer;
+import org.springframework.cache.jcache.config.JCacheConfigurerSupport;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig extends JCacheConfigurerSupport implements JCacheConfigurer {
+	
+	@Override
+	public CacheErrorHandler errorHandler()
+	{
+		return new CacheErrorHandler() {
+			
+			@Override
+			public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void handleCacheClearError(RuntimeException exception, Cache cache) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+	}
+
+}
