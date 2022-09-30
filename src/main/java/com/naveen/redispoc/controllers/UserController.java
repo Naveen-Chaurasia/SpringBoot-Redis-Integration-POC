8c14a221df4446bb940d12ed71665c0c0795baa8
Naveen <naveen.chaurasia861@gmail.com>
2022-09-30 10:08:46 +0530

    added a test scenario

--- src/main/java/com/naveen/redispoc/controllers/UserController.java
+++ src/main/java/com/naveen/redispoc/controllers/UserController.java
@@ -1,4 +1,5 @@
 package com.naveen.redispoc.controllers;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,13 +32,38 @@ public class UserController {
     return userRepository.findById(userId).get();
   }
   
-//  Both findById() and getOne() methods are used to retrieve an object from underlying datastore. But the underlying mechanism
-//  for retrieving records is different for both these methods, infact getOne() is lazy operation which does not even hit the database.
-  //https://www.javacodemonk.com/difference-between-getone-and-findbyid-in-spring-data-jpa-3a96c3ff
+
   @CachePut(value = "users", key = "#user.id")
   @PutMapping("/update")
   public User updatePersonByID(@RequestBody User user) {
     userRepository.save(user);
     return user;
   }
+  
+//Both findById() and getOne() methods are used to retrieve an object from underlying datastore. But the underlying mechanism
+//for retrieving records is different for both these methods, infact getOne() is lazy operation which does not even hit the database.
+//https://www.javacodemonk.com/difference-between-getone-and-findbyid-in-spring-data-jpa-3a96c3ff
+
+  
+  
+  
+  
+  
+//  @Test
+//  void
+//  givenRedisCaching_whenFindUserById_thenUserReturnedFromCache() {
+//     
+//
+//     User UserNotFromCache = userRepository.getUserById(id);
+//     User UserFromCache = userRepository.getUserById(id);
+//
+//     assertThat(UserNotFromCache).isEqualTo(User);
+//     assertThat(UserFromCache).isEqualTo(User);
+//
+//     verify(mockUserRepository, times(1)).findById(id);
+//     assertThat(UserFromCache()).isEqualTo(User);
+//  }
+  
+  
+  
 }
